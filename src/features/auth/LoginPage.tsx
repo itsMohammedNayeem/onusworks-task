bb1686fc239165e30a51ca68c3b283f7c57e435d
itsMohammedNayeem <nayeem.gmit@gmail.com>
2024-02-17 23:47:38 +0530

    Add autoprefixer, postcss dependencies and style login page

--- src/features/auth/LoginPage.tsx
+++ src/features/auth/LoginPage.tsx
@@ -46,8 +46,8 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 font-mono">
+      <h1 className="text-xl font-semibold mb-4">Login Page</h1>
 
       <Formik
         initialValues={{ username: "", password: "" }}
@@ -64,28 +64,63 @@ const LoginPage = () => {
         onSubmit={handleSubmit}
       >
         {({ isSubmitting }) => (
-          <Form>
-            <div>
-              <label htmlFor="username">Username</label>
-              <Field type="text" name="username" />
-              <ErrorMessage name="username" component="div" />
+          <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                htmlFor="username"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Username
+              </label>
+              <Field
+                type="text"
+                name="username"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+              <ErrorMessage
+                name="username"
+                component="div"
+                className="text-red-500 text-xs italic mb-4"
+              />
             </div>
 
-            <div>
-              <label htmlFor="password">Password</label>
-              <Field type="password" name="password" />
-              <ErrorMessage name="password" component="div" />
+            <div className="mb-4s">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Password
+              </label>
+              <Field
+                type="password"
+                name="password"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="text-red-500 text-xs italic mb-4"
+              />
             </div>
 
             {loginFailed && (
-              <div className="error">
+              <div
+                className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4"
+                role="alert"
+              >
                 Invalid login details. Please try again.
               </div>
             )}
 
-            <button type="submit" disabled={isSubmitting}>
-              Login
-            </button>
+            <div className="flex items-center justify-between">
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Login
+              </button>
+            </div>
           </Form>
         )}
       </Formik>
